288990d3380194c11373d8a2c0b62da7bd7e6656
alumno <alumno@328-23>
2016-10-03 12:34:03 -0500

    FP-Menu. Muestra de menu consola en java.

--- FP-Menú/src/fp/menú/FPMenú.java
+++ FP-Menú/src/fp/menú/FPMenú.java
@@ -15,18 +15,39 @@ public class FPMenú {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("La opción elegida es:" + menú());// 
     }
     
-}
 
 public static int menú(){
-int opcion;
-System.out.println("****  Cáculo de áreas  ****)";
-System.out.prinln("****  Menú  ****");
-System.ou.println("Introducza la opción deseada");
-System.out.print("Introduzca la opción deseada:");
-System.ou.println("Área de cuadrados");
-System.out.print("Área de círculos");
-System.out.print("Área de triángulos");
+    Scanner entrada = new Scanner(System.in);
+    int opcion;
+    do{
+    System.out.println("****  Cáculo de áreas  ****");
+    System.out.println("****  Menú  ****");
+    System.out.println("Introducza la opción deseada");
+    System.out.println("Introduzca la opción deseada:");
+    System.out.println("1.-Área de cuadrados.");
+    System.out.println("2.-Á1rea de círculos.");
+    System.out.println("3.-Área de triángulos.");
+     System.out.println("0.-Salir.");
+    opcion = entrada.nextInt();
+    }while(opcion<0 || opcion >= 3);
+    return opcion;
+    }
+
+public static float calculaAreas(int op){
+    float area;
+    switch(op){
+        case 1;
+            area = areaCuadrado();
+        case 2;
+            area = areaCírculo();
+         case 3;
+            area = areaTriángulo();
+        default;
+            mensajeSalida();
+    }
+    return area;
+}
 }
