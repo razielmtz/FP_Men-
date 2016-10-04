8e1523a273776cd9f3821840e20c2adeadc92c22
razielmtz <razielmartinezcastillo@gmail.com>
2016-10-03 21:50:25 -0500

    Update FPMenú.java

--- FP-Menú/src/fp/menú/FPMenú.java
+++ FP-Menú/src/fp/menú/FPMenú.java
@@ -15,39 +15,73 @@ public class FPMenú {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("La opción elegida es:" + menú());// 
+              System.out.println("La opcion elegida es: " +  menu());    
+    }
+    public static int menu(){
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        do{
+        System.out.println("**** Calculo de areas ****");
+        System.out.println("**** Menu ****");
+        System.out.println("1. Area de cuadrados");
+        System.out.println("2. Area de circulos");
+        System.out.println("3. Area de triangulos");
+        System.out.println("0. Salir");
+        System.out.print("Introdusca la opcion deseada: ");
+        opcion = entrada.nextInt();
+        }while(opcion<0 || opcion>3);
+        return opcion;
+    }
+    public static float calcularArea(int op){
+        float area;
+        switch (op){
+            case 1:
+                area = areaCuadrado();
+                break;
+            case 2:
+                area = areaCirculo();
+                break;
+            case 3:
+                area = areaTriangulo();
+                break;
+            default:
+                mensajeSalida();
+        }
+        return area;
+    }
+    public static double solicitarDatos(String tipo){
+        double dato;
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Introduzca el valor de " + tipo + " : ");
+        dato = entrada.nextDouble();
+        return dato;
+    }
+    public static float areaCuadrado(){
+        float area;
+        double a;
+        a = solicitarDatos("Introduce el valor de un lado");
+        area = (float) Math.pow(a, 2);
+        return area;
+        }
+    public static float areaCirculo(){
+        float area;
+        double r;
+        final double pi;
+        pi = 3.14;
+        r = solicitarDatos("Introduce el valor del radio");
+        area = (float) (pi*Math.pow(r, 2));
+        return area;
+    }
+    public static float areaTriangulo(){
+        float area;
+        float base;
+        float altura;
+        area = (base * altura)/2;
+                
+        return area;
+    }
+    public static void mensajeSalida() {
+       
     }
     
-
-public static int menú(){
-    Scanner entrada = new Scanner(System.in);
-    int opcion;
-    do{
-    System.out.println("****  Cáculo de áreas  ****");
-    System.out.println("****  Menú  ****");
-    System.out.println("Introducza la opción deseada");
-    System.out.println("Introduzca la opción deseada:");
-    System.out.println("1.-Área de cuadrados.");
-    System.out.println("2.-Á1rea de círculos.");
-    System.out.println("3.-Área de triángulos.");
-     System.out.println("0.-Salir.");
-    opcion = entrada.nextInt();
-    }while(opcion<0 || opcion >= 3);
-    return opcion;
-    }
-
-public static float calculaAreas(int op){
-    float area;
-    switch(op){
-        case 1:
-            area = areaCuadrado();
-        case 2:
-            area = areaCírculo();
-         case 3:
-            area = areaTriángulo();
-         default:
-            mensajeSalida();
-    }
-    return area;
-}
 }
